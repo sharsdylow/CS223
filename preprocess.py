94e8122008e689b925096f855b44ae720d8fe988
pokr <mistrpokr@outlook.com>
2023-05-13 20:55:53 -0700

    process queries (.sql) (tested with low concur observation)

--- preprocess.py
+++ preprocess.py
@@ -2,8 +2,13 @@
 from datetime import datetime
 import heapq
 
-# FILENAME = "dataset-example/queries-example.txt"
-FILENAME = "dataset/queries/low_concurrency/queries.txt"
+# QUERIES = "dataset-example/queries-example.txt"
+QUERIES = "dataset/queries/low_concurrency/queries.txt"
+SAVE_QUERIES = "dataset-processed/queries.txt"
+
+# OBSERVATION = "dataset-example/observation_example.sql"
+OBSERVATION = "dataset/data/low_concurrency/observation_low_concurrency.sql"
+
 DEFAULT_SCALE = 1440  # Compress 20 days -> 20 minutes
 TIMESTAMP_BASE = 1510099200  # Epoch time of 2017-11-08T00:00:00Z
 
@@ -19,10 +24,40 @@ def scale_ts_to_float(ts: str, scale=DEFAULT_SCALE, ts_base=TIMESTAMP_BASE) -> f
     return ts_float
 
 
-def main():
+def process_sql(filename=OBSERVATION):
     queries_list = []
-    with open(FILENAME, "r") as f:
-        sql_buffer = ""
+    with open(filename, "r") as f:
+        current_query = ""
+        ts = ""
+        ts_float = 0.0
+        start_of_inserts = 0
+
+        # Preserve the SET lines at the start
+        # Perform an initial scan for the start of INSERT lines
+        for idx, line in enumerate(f):
+            if line.startswith("INSERT"):
+                start_of_inserts = idx  # Record start of INSERTs
+                break
+
+        f.seek(0, 0)  # Go back to start of file
+        for _ in range(start_of_inserts):
+            next(f)  # Skip until start of INSERTS
+
+        for line in f:
+            try:
+                ts_iso = line.split("'")[-4]
+            except IndexError:
+                continue
+            ts_float = scale_ts_to_float(ts_iso)
+            heapq.heappush(queries_list, (ts_float, line))
+
+    return queries_list
+
+
+def process_queries(filename=QUERIES):
+    queries_list = []
+    with open(filename, "r") as f:
+        current_query = ""
         ts = ""
         ts_float = 0.0
 
@@ -34,15 +69,33 @@ def main():
                 ts = line[:-3]
                 ts_float = scale_ts_to_float(ts)
             elif '"' in line:
-                heapq.heappush(queries_list, (ts_float, sql_buffer))
-                sql_buffer = ""
+                heapq.heappush(queries_list, (ts_float, current_query))
+                current_query = ""
             else:
-                sql_buffer += line.strip("\n").strip("\t").strip(" ") + " "
+                current_query += line.strip("\n").strip("\t").strip(" ") + " "
     return queries_list
 
 
+def save_queries(queries: list[tuple[float, str]], save_file: str = SAVE_QUERIES):
+    with open(save_file, "w") as f:
+        for q in queries:
+            f.write(q[1])
+            f.write("\n")
+        f.close()
+
+
+def main():
+    queries = process_queries()
+    save_queries(queries)
+    while queries:
+        q = heapq.heappop(queries)
+        # print(q)
+
+    sqls = process_sql()
+    while sqls:
+        q = heapq.heappop(sqls)
+        # print(q)
+
+
 if __name__ == "__main__":
-    ret = main()
-    while ret:
-        q = heapq.heappop(ret)
-        print(q)
+    main()
