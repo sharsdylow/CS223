2b4b033b127a84bd477a0740e3104bf9104fcad0
Eve Zhang <xuyinz1@uci.edu>
2023-05-14 16:15:57 -0700

    modify port, add comments

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import psycopg
+import time
 
 DROP = "dataset/schema/drop.sql"
 CREAT = "dataset/schema/create.sql"
 METADATA_LOW = "dataset-processed/metadata_low_concurrency.sql"
 
-CONNECTION = "host=173.255.210.115 port=5432 dbname=postgres user = postgres password=example connect_timeout=10"
+CONNECTION = "host=173.255.210.115 port=55432 dbname=postgres user = postgres password=example connect_timeout=10"
 
 def execute_sql(filename: str, connection_string: str):
     cnt = 1024
@@ -29,12 +30,16 @@ def execute_sql(filename: str, connection_string: str):
                     cnt = 1024
 
 def main():
+    start = time.time()
     execute_sql(DROP, CONNECTION)
+    print("Delete all tables successfully.")
     # Init database
     # Create table
     execute_sql(CREAT, CONNECTION)
+    print("Create all tables successfully.")
     # Insert metadata
     execute_sql(METADATA_LOW, CONNECTION)
-
+    print(f"runtime: {time.time()-start}")
+    
 if __name__== "__main__":
     main()
