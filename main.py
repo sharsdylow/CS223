e1e003f74c3bd82abbd52e49aeae6ef86be83a2e
Eve Zhang <xuyinz1@uci.edu>
2023-05-14 15:07:17 -0700

    init database insert metadata

---
+++ main.py
@@ -0,0 +1,40 @@
+import psycopg
+
+DROP = "dataset/schema/drop.sql"
+CREAT = "dataset/schema/create.sql"
+METADATA_LOW = "dataset-processed/metadata_low_concurrency.sql"
+
+CONNECTION = "host=173.255.210.115 port=5432 dbname=postgres user = postgres password=example connect_timeout=10"
+
+def execute_sql(filename: str, connection_string: str):
+    cnt = 1024
+    # Connect to an existing database
+    with psycopg.connect(connection_string) as conn:
+        # Open a cursor to perform database operations
+        with conn.cursor() as cur:
+            fd = open(filename, 'r')
+            sqlFile = fd.read()
+            fd.close()
+            sqlCommands = sqlFile.split(';')
+            for command in sqlCommands:
+                try:
+                    cur.execute(command)
+                    cnt -= 1
+                except:
+                    print("Command skipped")
+                    continue
+                if cnt == 0:
+                    conn.commit()
+                    print("commit succeessfully")
+                    cnt = 1024
+
+def main():
+    execute_sql(DROP, CONNECTION)
+    # Init database
+    # Create table
+    execute_sql(CREAT, CONNECTION)
+    # Insert metadata
+    execute_sql(METADATA_LOW, CONNECTION)
+
+if __name__== "__main__":
+    main()
