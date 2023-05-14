0af4d81ab75659dd9e1a96cd61c5c37ed817cd85
pokr <mistrpokr@outlook.com>
2023-05-13 17:36:35 -0700

    scale timestamp

--- preprocess.py
+++ preprocess.py
@@ -1,12 +1,22 @@
 # Preprocess the dataset and sort in time order
+from datetime import datetime
 import heapq
 
 # FILENAME = "dataset-example/queries-example.txt"
 FILENAME = "dataset/queries/low_concurrency/queries.txt"
+DEFAULT_SCALE = 1440  # Compress 20 days -> 20 minutes
+TIMESTAMP_BASE = 1510099200  # Epoch time of 2017-11-08T00:00:00Z
 
 
-def scale_ts_to_float(timestamp: str):
-    return 0.0
+def scale_ts_to_float(ts: str, scale=DEFAULT_SCALE, ts_base=TIMESTAMP_BASE) -> float:
+    """
+    input: timestamp (str), precisely in the format of YYYY-MM-DDTHH:MM:SSZ
+    output: scaled timestamp in float (float)
+    """
+    ts_int = datetime.fromisoformat(ts).timestamp()
+    ts_float = (ts_int - ts_base) / scale
+
+    return ts_float
 
 
 def main():
@@ -21,7 +31,7 @@ def main():
                 # This line is a timestamp
                 # in ISO 8601 format:
                 # YYYY-MM-DDTHH:MM:SSZ,
-                ts = line[:-2]
+                ts = line[:-3]
                 ts_float = scale_ts_to_float(ts)
             elif '"' in line:
                 heapq.heappush(queries_list, (ts_float, sql_buffer))
