4b42fbe86077558136a0f103d656bcc5658b855b
Eve Zhang <xuyinz1@uci.edu>
2023-05-19 14:10:32 -0700

    move worker out of execute_queries read file only once

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ METADATA_LOW = "dataset-processed/metadata_low_concurrency.sql"
 QUERIES_LOW = "dataset-processed/queries_low_concurrency.sql"
 EXAMPLE = "dataset-example/example.sql"
 
-TRANSACTION_SIZE = 2048
+TRANSACTION_SIZE = 100
 THREADS_NUM = 4  # Multi Processing Level
 
 CONNECTION_STRING = "host=localhost port=55432 dbname=postgres user = postgres password=example connect_timeout=10"
@@ -27,52 +27,49 @@ def execute_sql(filename: str):
                 cur.execute(command)
             conn.commit()
 
+def worker(thread_id, thread_queries):
+    count, executed = 0, 0
+    total = ceil(len(thread_queries)/TRANSACTION_SIZE)
+    conn = psycopg.connect(CONNECTION_STRING)
+    print(f"Thread {thread_id} connected to the database")
+    try:
+        with conn.cursor() as cursor:
+            for query in thread_queries:
+                if query.strip():
+                    cursor.execute(query)
+                    executed += 1
+                if executed == TRANSACTION_SIZE:
+                    conn.commit()
+                    executed = 0
+                    count += 1
+                    print(f"Thread {thread_id} executed a transaction ({count}/{total})")
+    except psycopg.Error as e:
+        conn.rollback()
+        print(f"Error executing transaction in thread {thread_id}: {e}")         
+    conn.close()
+    print(f"Thread {thread_id} disconnected from the database")
 
 def execute_queries(filename: str):
-    def worker(thread_id, filename):
-        conn = psycopg.connect(CONNECTION_STRING)
-        print(f"Thread {thread_id} connected to the database")
-
-        with open(filename) as sql_file:
-            queries = sql_file.read().split(";")
-
-        start = thread_id * TRANSACTION_SIZE
-        end = start + TRANSACTION_SIZE
-        count = 0
-        total = ceil(len(queries) / (TRANSACTION_SIZE * THREADS_NUM))
-
-        while start < len(queries):
-            transaction_queries = queries[start:end]
-
-            try:
-                with conn.cursor() as cursor:
-                    for query in transaction_queries:
-                        if query.strip():
-                            cursor.execute(query)
-                    conn.commit()
-            except psycopg.Error as e:
-                conn.rollback()
-                print(f"Error executing transaction in thread {thread_id}: {e}")
-            else:
-                print(f"Thread {thread_id} executed a transaction ({count}/{total})")
-
-            start += THREADS_NUM * TRANSACTION_SIZE
-            end += THREADS_NUM * TRANSACTION_SIZE
-            count += 1
-
-        conn.close()
-        print(f"Thread {thread_id} disconnected from the database")
-
     processes = []
 
-    for i in range(THREADS_NUM):
-        p = Process(target=worker, args=(i, filename))
-        processes.append(p)
-        p.start()
+    with open(filename) as sql_file:
+        queries = sql_file.read().split(";")
+        # init every thread
+        for thread_id in range(THREADS_NUM):
+            thread_queries = []
+            start = thread_id * TRANSACTION_SIZE
+            end = start + TRANSACTION_SIZE
+            while start < len(queries):
+                thread_queries += queries[start:end]    # get queries for each thread
+                start += THREADS_NUM * TRANSACTION_SIZE
+                end += THREADS_NUM * TRANSACTION_SIZE
+
+            p = Process(target=worker, args=(thread_id, thread_queries))
+            processes.append(p)
+            p.start()
 
     for process in processes:
         process.join()
-
 
 def main():
     start = time.time()
@@ -90,7 +87,7 @@ def main():
     print(f"Metadata runtime: {meta_t-start}")
 
     # execute queries
-    execute_queries(QUERIES_LOW)
+    execute_queries(EXAMPLE)
     # execute_queries(EXAMPLE)
     # execute_sql(QUERIES_LOW, CONNECTION)
     print(f"Queries runtime: {time.time()-meta_t}")
