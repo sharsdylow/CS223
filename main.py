74ab951214ed0850d2f0dd76329aa33e193a0d09
Eve Zhang <xuyinz1@uci.edu>
2023-05-20 00:20:24 -0700

    Add avg queries response time and whole workload response time calculation

--- main.py
+++ main.py
@@ -10,14 +10,13 @@ METADATA_LOW = "dataset-processed/metadata_low_concurrency.sql"
 QUERIES_LOW = "dataset-processed/queries_low_concurrency.sql"
 EXAMPLE = "dataset-example/example.sql"
 
-TRANSACTION_SIZE = 100
-THREADS_NUM = 4  # Multi Processing Level
+TRANSACTION_SIZE = 64
+THREADS_NUM = 100 # Multi Processing Level
 
 CONNECTION_STRING = "host=localhost port=55432 dbname=postgres user = postgres password=example connect_timeout=10"
 
 
 def execute_sql(filename: str):
-    # Connect to an existing database
     with open(filename, "r") as fd:
         sqlCommands = fd.read().split(";")
 
@@ -27,7 +26,7 @@ def execute_sql(filename: str):
                 cur.execute(command)
             conn.commit()
 
-def worker(thread_id, queries):
+def worker(thread_id, queries, response_time):
     conn = psycopg.connect(CONNECTION_STRING)
     print(f"Thread {thread_id} connected to the database")
 
@@ -41,7 +40,10 @@ def worker(thread_id, queries):
             with conn.cursor() as cursor:
                 for query in transaction_queries:
                     if query.strip():
+                        start_t = time.time()
                         cursor.execute(query)
+                        end_t = time.time()
+                        response_time.append(end_t - start_t)
                 conn.commit()
                 count += 1 
         except psycopg.Error as e:
@@ -58,18 +60,23 @@ def worker(thread_id, queries):
 
 def execute_queries(filename: str):
     processes = []
+    response_time = Manager().list()
 
     with open(filename) as sql_file:
         queries = Manager().list()
         queries += sql_file.read().split(";")
         # init every thread
         for thread_id in range(THREADS_NUM):
-            p = Process(target=worker, args=(thread_id, queries))
+            p = Process(target=worker, args=(thread_id, queries, response_time))
             processes.append(p)
             p.start()
 
     for process in processes:
         process.join()
+
+    avg_response_time = sum(response_time) / len(response_time)
+    print(f"Average response time for queries: {avg_response_time:.4f} seconds")
+
 
 def main():
     start = time.time()
@@ -83,14 +90,13 @@ def main():
     # Insert metadata
     execute_sql(METADATA_LOW)
     print("Insert all metadata successfully.")
-    meta_t = time.time()
-    print(f"Metadata runtime: {meta_t-start}")
-
+    
     # execute queries
-    execute_queries(EXAMPLE)
+    start = time.time()
+    execute_queries(QUERIES_LOW)
     # execute_queries(EXAMPLE)
-    # execute_sql(QUERIES_LOW, CONNECTION)
-    print(f"Queries runtime: {time.time()-meta_t}")
+    response_time = time.time() - start
+    print(f"Average response time of the whole workload: {response_time:.4f} seconds")
 
 
 if __name__ == "__main__":
