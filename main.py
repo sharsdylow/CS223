e92e3f87c6cc1cde69542a775edddcefd15264aa
pokr <mistrpokr@outlook.com>
2023-05-21 21:18:17 -0700

    parse cli options with argparse
    getting ready for test automation

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ import time
 from multiprocessing import Manager, Process
 from math import ceil, floor
 from psycopg import Connection
+import argparse
 
 DROP = "dataset/schema/drop.sql"
 CREAT = "dataset/schema/create.sql"
-METADATA_LOW = "dataset-processed/metadata_low_concurrency.sql"
-QUERIES_LOW = "dataset-processed/queries_low_concurrency.sql"
+METADATA = "dataset-processed/metadata_low_concurrency.sql"
+QUERIES = "dataset-processed/queries_low_concurrency.sql"
 EXAMPLE = "dataset-example/example.sql"
 
 TRANSACTION_SIZE = 64
-THREADS_NUM = 4 # Multi Processing Level
+THREADS_NUM = 4  # Multi Processing Level
 
 CONNECTION_STRING = "host=localhost port=55432 dbname=postgres user = postgres password=example connect_timeout=10"
 
@@ -26,16 +27,17 @@ def execute_sql(filename: str):
                 cur.execute(command)
             conn.commit()
 
+
 def worker(thread_id, queries, avg_response_time):
     conn = psycopg.connect(CONNECTION_STRING)
     print(f"Thread {thread_id} connected to the database")
 
     transaction_count = 0
     query_count = 0
-    total = ceil(len(queries)/TRANSACTION_SIZE)
-    
+    total = ceil(len(queries) / TRANSACTION_SIZE)
+
     total_response_time = 0.0
-    
+
     try:
         with conn.cursor() as cursor:
             for query in queries:
@@ -44,24 +46,27 @@ def worker(thread_id, queries, avg_response_time):
                     cursor.execute(query)
                     end_t = time.time()
                     query_count += 1
-                    total_response_time += (end_t - start_t)
+                    total_response_time += end_t - start_t
 
-                if query_count == TRANSACTION_SIZE:                        
+                if query_count == TRANSACTION_SIZE:
                     conn.commit()
-                    transaction_count += 1 
+                    transaction_count += 1
                     query_count = 0
-                    print(f"Thread {thread_id} executed a transaction ({transaction_count}/{total})")
+                    print(
+                        f"Thread {thread_id} executed a transaction ({transaction_count}/{total})"
+                    )
 
     except psycopg.Error as e:
         conn.rollback()
-        print(f"Error executing transaction in thread {thread_id}: {e}")    
+        print(f"Error executing transaction in thread {thread_id}: {e}")
 
     conn.close()
     # avg_response_time[thread_id] = avg
     avg_response_time[thread_id] = total_response_time / len(queries)
     print(f"Thread {thread_id} disconnected from the database")
 
-def execute_queries(filename: str):
+
+def execute_sql_concurrent(filename: str):
     start = time.time()
     processes = []
     avg_response_time = Manager().list([0 for _ in range(THREADS_NUM)])
@@ -77,11 +82,16 @@ def execute_queries(filename: str):
             end = start + TRANSACTION_SIZE
             while start < len(queries):
                 # queries_slice += queries[start:end]    # get queries for each thread
-                query_slices[thread_id] += queries[start:end]    # get queries for each thread
+                query_slices[thread_id] += queries[
+                    start:end
+                ]  # get queries for each thread
                 start += THREADS_NUM * TRANSACTION_SIZE
-                end += THREADS_NUM * TRANSACTION_SIZE   
+                end += THREADS_NUM * TRANSACTION_SIZE
             # p = Process(target=worker, args=(thread_id, queries_slice, avg_response_time))
-            p = Process(target=worker, args=(thread_id, query_slices[thread_id], avg_response_time))
+            p = Process(
+                target=worker,
+                args=(thread_id, query_slices[thread_id], avg_response_time),
+            )
             processes.append(p)
             p.start()
 
@@ -103,8 +113,6 @@ def execute_queries(filename: str):
     print(f"Average response time for queries: {avg_queries_time_1:.8f} seconds.")
 
 
-
-
 def main():
     start = time.time()
 
@@ -115,15 +123,45 @@ def main():
     execute_sql(CREAT)
     print("Create all tables successfully.")
     # Insert metadata
-    execute_sql(METADATA_LOW)
+    execute_sql(METADATA)
     print("Insert all metadata successfully.")
-    
+
     # execute queries
-    execute_queries(QUERIES_LOW)
+    execute_sql_concurrent(QUERIES)
     # execute_queries(EXAMPLE)
 
 
-
-
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Test concurrent transactions with PostgreSQL. "
+    )
+    parser.add_argument(
+        "--size", metavar="T", type=int, help="Transaction size", default=16
+    )
+    parser.add_argument(
+        "--workers",
+        metavar="W",
+        type=int,
+        help="Number of workers (client threads)",
+        default=64,
+    )
+    parser.add_argument(
+        "--metadata",
+        type=str,
+        help="A file containing all database metadata",
+        required=True,
+    )
+    parser.add_argument(
+        "--queries",
+        type=str,
+        help="A file containing all database queries",
+        required=True,
+    )
+
+    args = parser.parse_args()
+    TRANSACTION_SIZE = args.size
+    THREADS_NUM = args.workers
+    METADATA = args.metadata
+    QUERIES = args.queries
+
     main()
