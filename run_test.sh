39f8089a009b48786c234bf89947b6f8185614da
pokr <mistrpokr@outlook.com>
2023-05-21 23:43:59 -0700

    handle high concurrency files

--- run_test.sh
+++ run_test.sh
@@ -3,7 +3,8 @@
 VERBOSE=1
 
 DIR_LOG="logs"
-LOG_FILE="$DIR_LOG/db-test-$(date --iso-8601=seconds).log"
+LOG_FILE_LOW="$DIR_LOG/low-$(date --iso-8601=seconds).log"
+LOG_FILE_HIGH="$DIR_LOG/high-$(date --iso-8601=seconds).log"
 
 DIR_PROCESSED="dataset-processed"
 METADATA_LOW="${DIR_PROCESSED}/metadata_low_concurrency.sql"
@@ -19,8 +20,25 @@ CONSISTENCY_LEVEL=(1 2 3 4)
 
 # Create empty file
 mkdir -p $DIR_LOG
-touch "$LOG_FILE"
-echo -e "level\tmpl\tsize\tthroughput\tquery\tworkload" >>"$LOG_FILE"
+touch "$LOG_FILE_LOW"
+touch "$LOG_FILE_HIGH"
+echo -e "level\tmpl\tsize\tthroughput\tquery\tworkload" >>"$LOG_FILE_LOW"
+echo -e "level\tmpl\tsize\tthroughput\tquery\tworkload" >>"$LOG_FILE_HIGH"
+
+# # Run tests on low concurrency dataset
+# for lvl in "${CONSISTENCY_LEVEL[@]}"; do
+#     for mpl in "${MPL[@]}"; do
+#         for size in "${TRANSACTION_SIZE[@]}"; do
+#             if [[ VERBOSE -eq 1 ]]; then
+#                 echo "Running test under setup: CONSISTENCY_LEVEL=$lvl MPL=$mpl TRANSACTION_SIZE=$size"
+#                 cmd="python main.py --truncate 1000 --consistency-level $lvl --size $size --workers $mpl --metadata $METADATA_LOW --queries $QUERY_LOW"
+#                 echo $cmd
+#                 echo -e -n "$lvl\t$mpl\t$size\t" >>"$LOG_FILE_LOW"
+#                 $cmd | tail -n 1 >>"$LOG_FILE_LOW"
+#             fi
+#         done
+#     done
+# done
 
 # Run tests on low concurrency dataset
 for lvl in "${CONSISTENCY_LEVEL[@]}"; do
@@ -28,10 +46,10 @@ for lvl in "${CONSISTENCY_LEVEL[@]}"; do
         for size in "${TRANSACTION_SIZE[@]}"; do
             if [[ VERBOSE -eq 1 ]]; then
                 echo "Running test under setup: CONSISTENCY_LEVEL=$lvl MPL=$mpl TRANSACTION_SIZE=$size"
-                cmd="python main.py --truncate 1000 --consistency-level $lvl --size $size --workers $mpl --metadata $METADATA_LOW --queries $QUERY_LOW"
+                cmd="python main.py --truncate 1000 --consistency-level $lvl --size $size --workers $mpl --metadata $METADATA_HIGH --queries $QUERY_HIGH"
                 echo $cmd
-                echo -e -n "$lvl\t$mpl\t$size\t" >>"$LOG_FILE"
-                $cmd | tail -n 1 >>"$LOG_FILE"
+                echo -e -n "$lvl\t$mpl\t$size\t" >>"$LOG_FILE_HIGH"
+                $cmd | tail -n 1 >>"$LOG_FILE_HIGH"
             fi
         done
     done
