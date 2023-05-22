477b69157c9c47f156a3657938c47c7b7326e71a
pokr <mistrpokr@outlook.com>
2023-05-21 23:32:44 -0700

    add consistency level;add table header

--- run_test.sh
+++ run_test.sh
@@ -15,20 +15,24 @@ QUERY_HIGH="${DIR_PROCESSED}/queries_high_concurrency.sql"
 # Test conditions
 MPL=(1 4 16 128) # Multi programming level
 TRANSACTION_SIZE=(4 16 128 1024)
+CONSISTENCY_LEVEL=(1 2 3 4)
 
 # Create empty file
 mkdir -p $DIR_LOG
 touch "$LOG_FILE"
+echo -e "level\tmpl\tsize\tthroughput\tquery\tworkload" >>"$LOG_FILE"
 
 # Run tests on low concurrency dataset
-
-for mpl in "${MPL[@]}"; do
-    for size in "${TRANSACTION_SIZE[@]}"; do
-        if [[ VERBOSE -eq 1 ]]; then
-            echo "Running test under setup: MPL=$mpl TRANSACTION_SIZE=$size"
-            cmd="python main.py --truncate 1000 --size $size --workers $mpl --metadata $METADATA_LOW --queries $QUERY_LOW"
-            echo $cmd
-            $cmd | tail -n 1 >> "$LOG_FILE"
-        fi
+for lvl in "${CONSISTENCY_LEVEL[@]}"; do
+    for mpl in "${MPL[@]}"; do
+        for size in "${TRANSACTION_SIZE[@]}"; do
+            if [[ VERBOSE -eq 1 ]]; then
+                echo "Running test under setup: CONSISTENCY_LEVEL=$lvl MPL=$mpl TRANSACTION_SIZE=$size"
+                cmd="python main.py --truncate 1000 --consistency-level $lvl --size $size --workers $mpl --metadata $METADATA_LOW --queries $QUERY_LOW"
+                echo $cmd
+                echo -e -n "$lvl\t$mpl\t$size\t" >>"$LOG_FILE"
+                $cmd | tail -n 1 >>"$LOG_FILE"
+            fi
+        done
     done
 done
